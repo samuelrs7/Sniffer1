8e572b9f7c417392717adf113bffb7901accb2a3
Ubuntu <ubuntu@ip-172-31-48-18.ec2.internal>
2023-03-08 22:37:01 -0500

    Con la modificación de conexión a base de datos

--- servidor.js
+++ servidor.js
@@ -43,3 +43,20 @@ const serverHttp = http.createServer((req, res) => {
 serverHttp.listen(80, () => {
   console.log('HTTP server listening on port 80');
 });
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'mysql1.czemchtiopw1.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'prueba123',
+  database: 'mysql1'
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database: ' + error.stack);
+    return;
+  }
+  console.log('Connected to MySQL database with id ' + connection.threadId);
+});
