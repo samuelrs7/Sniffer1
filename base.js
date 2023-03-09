d00a823f6abc2ba4870c9f01e09cecd845d9c638
Ubuntu <ubuntu@ip-172-31-48-18.ec2.internal>
2023-03-08 22:28:34 -0500

    Conexión MySQL

---
+++ base.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'mysql1.czemchtiopw1.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'prueba123',
+  database: 'mysql1'
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database: ' + error.stack);
+    return;
+  }
+  console.log('Connected to MySQL database with id ' + connection.threadId);
+});
